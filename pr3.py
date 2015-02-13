9406857280eaab122c987fc65620018f0989dcf2
Bryan Allen <bryanpallen90@gmail.com>
2015-02-12 23:51:39 -0600

    regex matching for input strings

--- pr3.py
+++ pr3.py
@@ -42,22 +42,31 @@ if __name__ == "__main__":
 	PAIRS = (['b', 'p'], ['c', 'k'], ['c', 's'], ['d', 't'], ['e', 'y'], ['g', 'j'], ['g', 'k'], ['i', 'y'], ['k', 'q'], ['m', 'n'], ['s', 'z'], ['v', 'w'])
 	
 	# get input
-	#s1 = raw_input('string 1: ')
-	#s2 = raw_input('string 2: ')
-	s1 = 'selgect'
-	s2 = 'salekt'
+	reg = re.compile('[a-z]+')
+	
+	s1 = ''
+	r = reg.match(s1)
+	while not r or not r.group() == s1:
+		s1 = raw_input('string 1: ')
+		r = reg.match(s1)
+		
+	s2 = ''
+	r = reg.match(s2)
+	while not r or not r.group() == s2:
+		s2 = raw_input('string 2: ')
+		r = reg.match(s2)
 	
 	# create matrix
 	matrix = [[x for x in range(len(s2)+1)] for y in range(len(s1)+1)]
 	
-	# initialize
+	# initialize matrix
 	matrix[0][0] = Node(0, '', None)
 	for i in range(1, len(s1)+1):
 		matrix[i][0] = Node(i*GAP, 'H', [i-1, 0])
 	for j in range(1, len(s2)+1):
 		matrix[0][j] = Node(j*GAP, 'V', [0, j-1])
 		
-	# build
+	# build matrix
 	for i in range(1, len(s1)+1):
 		for j in range(1, len(s2)+1):
 			score = getScore(s1[i-1], s2[j-1])
